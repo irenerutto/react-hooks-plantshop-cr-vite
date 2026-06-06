21e5585940cb3ed01d847e6e406d8889015934c6
IRENE <irenerutto@gmail.com>
2026-06-06 08:01:57 +0300

    plant shop lab first commit

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+    // Local state to track stock status 
+  const [inStock, setInStock] = useState(true);
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+        {/* Plant image */}
+      <img src={plant.image} alt={plant.name} />
+
+      <h4>{plant.name}</h4>
+
+      <p>Price: {plant.price}</p>
+        {/* Toggle stock status */}
+      {inStock ? (
+        <button
+          className="primary"
+          onClick={() => setInStock(false)}
+        >
+          In Stock
+        </button>
       ) : (
         <button>Out of Stock</button>
       )}
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  // State to store all plants fetched from backend
+  const [plants, setPlants] = useState([]);
+  //state to store search input value
+  const [search, setSearch] = useState("");
+//fetch plants from backend when component first loads
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data)); // save fetched plants into state
+  }, []);
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+        {/* Form for adding a new plant */}
+      <NewPlantForm plants={plants} setPlants={setPlants} />
+        {/* Search input for filtering plants */}
+      <Search search={search} setSearch={setSearch} />
+      {/* List of plants filtered / full list */}
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
